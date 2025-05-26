e2170def704b041406e25380429b00880f1d39fc
CuB1z <cub1zdev@gmail.com>
2025-05-26 15:39:08 +0200

    feat: Implement FilePath class to manage default file paths for passwords and master password based on the operating system

--- src/main/java/com/cub1z/pwmanager/config/FilePath.java
+++ src/main/java/com/cub1z/pwmanager/config/FilePath.java
@@ -3,9 +3,31 @@ package com.cub1z.pwmanager.config;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PwdFilePath {
-    public static Path getDefault() {
-        String fileName = "passwords.pwd";
+public class FilePath {
+    private static final String PASSWORDS_FILE = "passwords.pwd";
+    private static final String MASTER_PASSWORD_FILE = "master.pwd";
+
+    /**
+     * Returns the default path for the passwords file.
+     * The path is determined based on the operating system.
+     *
+     * @return Path to the passwords file.
+     */
+    public static Path getPasswordsDefault() {
+        return getDefault(PASSWORDS_FILE);
+    }
+
+    /**
+     * Returns the default path for the master password file.
+     * The path is determined based on the operating system.
+     *
+     * @return Path to the master password file.
+     */
+    public static Path getMasterPasswordDefault() {
+        return getDefault(MASTER_PASSWORD_FILE);
+    }
+
+    private static Path getDefault(String fileName) {
         String os = System.getProperty("os.name").toLowerCase();
         Path baseDir;
 
